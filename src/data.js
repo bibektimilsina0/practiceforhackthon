69c07af45a2e87273fd975a2fb5839afee37fde0
bikkibro <bibektimilsina7857@gmail.com>
2023-06-28 20:49:22 +0545

    data

--- src/data.js
+++ src/data.js
@@ -1,42 +1,56 @@
-const products = [
+[
   {
-    id: 1,
-    name: 'albany sofa',
-    image:
-      'https://dl.airtable.com/.attachments/6ac7f7b55d505057317534722e5a9f03/9183491e/product-3.jpg',
-    price: 39.95,
-    desc: `I'm baby direct trade farm-to-table hell of, YOLO readymade raw denim venmo whatever organic gluten-free kitsch schlitz irony af flexitarian.`,
+      "id": 1,
+      "cid": "col1",
+      "name": "Pokhara",
+      "rid":"rendercol1",
+      "desc": "Most Beautiful place of Nepal ",
+      "video": "https://static.vecteezy.com/system/resources/previews/006/189/794/vr360-view-of-old-abandoned-factory-video.webm",
+      "image": "../culture.jpeg"
   },
   {
-    id: 2,
-    name: 'entertainment center',
-    image:
-      'https://dl.airtable.com/.attachments/da5e17fd71f50578d525dd5f596e407e/d5e88ac8/product-2.jpg',
-    price: 29.98,
-    desc: `I'm baby direct trade farm-to-table hell of, YOLO readymade raw denim venmo whatever organic gluten-free kitsch schlitz irony af flexitarian.`,
+      "id": 2,
+      "cid": "col2",
+      "name": "Kushma",
+      "rid":"rendercol1",
+      "desc": "Highest Bridge of Nepal ",
+      "video": "https://static.vecteezy.com/system/resources/previews/006/189/794/vr360-view-of-old-abandoned-factory-video.webm",
+      "image": "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579"
   },
   {
-    id: 3,
-    name: 'albany sectional',
-    image:
-      'https://dl.airtable.com/.attachments/05ecddf7ac8d581ecc3f7922415e7907/a4242abc/product-1.jpeg',
-    price: 10.99,
-    desc: `I'm baby direct trade farm-to-table hell of, YOLO readymade raw denim venmo whatever organic gluten-free kitsch schlitz irony af flexitarian.`,
+      "id": 3,
+      "cid": "col3",
+      "name": "Pashupatinath",
+      "rid":"rendercol1",
+      "desc": "Most Beautiful place of Nepal ",
+      "video": "https://static.vecteezy.com/system/resources/previews/006/189/794/vr360-view-of-old-abandoned-factory-video.webm",
+      "image": "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579"
   },
   {
-    id: 4,
-    name: 'leather sofa',
-    image:
-      'https://dl.airtable.com/.attachments/3245c726ee77d73702ba8c3310639727/f000842b/product-5.jpg',
-    price: 9.99,
-    desc: `I'm baby direct trade farm-to-table hell of, YOLO readymade raw denim venmo whatever organic gluten-free kitsch schlitz irony af flexitarian.`,
+      "id": 4,
+      "cid": "col4",
+      "name": "Pokhara",
+      "rid":"rendercol2",
+      "desc": "Most Beautiful place of Nepal ",
+      "video": "https://static.vecteezy.com/system/resources/previews/006/189/794/vr360-view-of-old-abandoned-factory-video.webm",
+      "image": "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579"
   },
+  {
+      "id": 5,
+      "cid": "col5",
+      "name": "Kushma",
+      "rid":"rendercol2",
+      "desc": "Highest Bridge of Nepal ",
+      "video": "https://static.vecteezy.com/system/resources/previews/006/189/794/vr360-view-of-old-abandoned-factory-video.webm",
+      "image": "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579"
+  },
+  {
+      "id": 6,
+      "cid": "col6",
+      "name": "Pashupatinath",
+      "rid":"rendercol2",
+      "desc": "Most Beautiful place of Nepal ",
+      "video": "https://s.bepro11.com/vr-video-sample.mp4",
+      "image": "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579"
+  }
 ]
-const people = [
-  { id: 1, name: 'john' },
-  { id: 2, name: 'peter' },
-  { id: 3, name: 'susan' },
-  { id: 4, name: 'anna' },
-  { id: 5, name: 'emma' },
-]
-module.exports = { products, people }
